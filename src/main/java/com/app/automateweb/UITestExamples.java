0f066f213cf1665042b00a1bb2205e77b47b93dc
Shivkumar Krishnan <team@codediy.io>
2022-08-21 10:10:39 +0800

    committing the change to the UI automation test file to add 3 test methods for the survey form page

--- src/main/java/com/app/automateweb/UITestExamples.java
+++ src/main/java/com/app/automateweb/UITestExamples.java
@@ -41,14 +41,80 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class UITestExamples {
 
-	protected WebDriver driver;
+	protected WebDriver driver;  // class "webdriver"   and object "driver "
+	@Test
+	public void submitbuttondisabled()throws InterruptedException{  
+		
+		System.setProperty ("webdriver.chrome.driver","C:\\chromedriver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		
+		driver.get("https://devexpress.github.io/testcafe/example/");
+		
+		WebElement submit = driver.findElement(By.id("submit-button"));	
+		boolean isEnabled = submit.isEnabled();
+
+		AssertJUnit.assertEquals(false ,isEnabled);
+		
+	WebElement element = driver.findElement(By.id("developer-name"));  // find element by id = dev name
+		String name = "Kanc";
+		element.sendKeys(name);
+		
+		isEnabled = submit.isEnabled();
+
+		AssertJUnit.assertEquals(true ,isEnabled);
+		submit.click();
+}
+
 	
 	@Test
-	public void clickSubmitButtonWithValues() throws InterruptedException 
+	public void featuresareimportant()throws InterruptedException {
+		
+		
+		System.setProperty ("webdriver.chrome.driver","C:\\chromedriver\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://devexpress.github.io/testcafe/example/");
+		
+		WebElement element = driver.findElement(By.id("developer-name"));  // find element by id = dev name
+		String name = "kanch";
+		element.sendKeys(name);
+		
+		
+		WebElement remote_testing_checkbox = driver.findElement(By.id("remote-testing"));   // 
+
+		remote_testing_checkbox.click();
+		
+		WebElement reusing_testing_checkbox = driver.findElement(By.id("reusing-js-code")); // 
+
+		reusing_testing_checkbox.click();
+		
+		WebElement submit = driver.findElement(By.id("submit-button"));	
+		submit.click();
+		
+		
+		WebElement background_parallel_testing_checkbox = driver.findElement(By.id("background_parallel_testing_checkbox")); // 
+
+		background_parallel_testing_checkbox.click();
+		
+		
+		WebElement continuous_integration_embedding_checkbox = driver.findElement(By.id("continuous_integration_embedding_checkbox")); // 
+
+		continuous_integration_embedding_checkbox.click();
+		
+		
+		WebElement traffic_markup_analysis_checkbox = driver.findElement(By.id("traffic_markup_analysis_checkbox"));           // 
+
+		traffic_markup_analysis_checkbox.click();
+		
+		;
+	}
+	
+	@Test   //method
+	public void clickSubmitButtonWithValues() throws InterruptedException  // method clickSubmitButtonWithValues
 	{
-	System.setProperty ("webdriver.chrome.driver","C:\\chromedriver\\chromedriver.exe");
+	System.setProperty ("webdriver.chrome.driver","C:\\chromedriver\\chromedriver.exe"); //C:\chromedriver , key value 
 	
 	driver = new ChromeDriver();
+	
 	
 	//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     //driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(30));
@@ -61,31 +127,29 @@ public class UITestExamples {
         .ignoring(NoSuchElementException.class);
 	 */
 	
-	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-		
 	// launch Chrome and redirect it to the Base URL
-	driver.get("https://devexpress.github.io/testcafe/example/");
+	driver.get("https://devexpress.github.io/testcafe/example/");   // get URL
 		
 	//Maximizes the browser window
-	driver.manage().window().maximize();
+	driver.manage().window().maximize();       // max window
 
 	
 	//get the actual value of the title
-	String title = driver.getTitle();
+	String title = driver.getTitle();                 // get title 
 	
-	System.out.println("page title="+ title);
+	System.out.println("page title="+ title);                  //print 
 		
-	WebElement element = driver.findElement(By.id("developer-name"));		
+	WebElement element = driver.findElement(By.id("developer-name"));  // find element by id = dev name
 	String name = "shiv";
 	element.sendKeys(name);
 	
-	WebElement triedTestCafeCheckbox = driver.findElement(By.id("tried-test-cafe"));
+	WebElement triedTestCafeCheckbox = driver.findElement(By.id("tried-test-cafe")); // 
 	triedTestCafeCheckbox.click();
 	
 	  WebElement slider = driver.findElement(By.id("slider"));
 		  
-	  JavascriptExecutor js = (JavascriptExecutor) driver;		
-	  js.executeScript("arguments[0].setAttribute('style', 'left: 33.33%;')",slider);
+		JavascriptExecutor js = (JavascriptExecutor) driver;		
+		js.executeScript("arguments[0].setAttribute('style', 'left: 33.33%;')",slider); // TODO
 		
 		/*
       Actions move = new Actions(driver);
@@ -102,30 +166,16 @@ public class UITestExamples {
 	Select preferred_interface_DropDown = new Select(driver.findElement(By.id("preferred-interface")));			
 	preferred_interface_DropDown.selectByIndex(1);
 	
-	WebElement submitButton = driver.findElement(By.id("submit-button"));	
-	submitButton.click();
+	WebElement submit = driver.findElement(By.id("submit-button"));	
+	submit.click();
 	
-	List<WebElement> elementList = driver.findElements(By.className("className"));
-	
-	
- 	//WebElement header = driver.findElement(By.id("article-header"));
-	
-	
-	WebDriverWait wait2=new WebDriverWait(driver, Duration.ofSeconds(5));
-	
-	WebElement header =
-	
-	wait2.until(ExpectedConditions.visibilityOfElementLocated(By.id("article-header")));
-			
-			
+	WebElement header = driver.findElement(By.id("article-header"));
 	String text = header.getText();
 	System.out.println(text);
 	
-	AssertJUnit.assertEquals(text, "Thank you, " + name + "!");
-		  
-	boolean e = false;
+	AssertJUnit.assertEquals( "Thank you, " + name + "!", text); // validation of name we have entered 
 	
-	AssertJUnit.assertEquals(false, e);
+	  
 	//close browser
 	driver.close();
 }
